322d25aa7fcf5b2fa76ad6a2c7c20ed8c1513fd6
GaoChengQuan <529390053@qq.com>
2017-09-10 09:53:35 +0800

    提交打印

--- src/com/situ/git/HelloGit.java
+++ src/com/situ/git/HelloGit.java
@@ -3,5 +3,9 @@ package com.situ.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit.main()");
+		System.out.println("HelloGit.main()");
+		System.out.println("HelloGit.main()");
+		System.out.println("HelloGit.main()");
+		System.out.println("HelloGit.main()");
 	}
 }
